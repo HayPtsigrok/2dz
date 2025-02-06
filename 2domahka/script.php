aaebffdc6cd1cb5b5fc69b25789b134d6276bfa1
HayPtsigrok <148876334+HayPtsigrok@users.noreply.github.com>
2025-02-06 22:33:10 +0300

    Update script.php

--- 2domahka/script.php
+++ 2domahka/script.php
@@ -1,20 +1,21 @@
 <?php
-$messages = [];
-$filePath = 'data.txt';
+$messages = []; // Инициализация пустого массива для хранения сообщений.
+$filePath = 'data.txt'; // Указание пути к файлу, в который будут сохраняться сообщения.
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = htmlspecialchars(trim($_POST['name']));
-    $email = htmlspecialchars(trim($_POST['email']));
-    $message = htmlspecialchars(trim($_POST['message']));
+if ($_SERVER["REQUEST_METHOD"] == "POST") { // Проверка, был ли отправлен POST-запрос.
+    $name = htmlspecialchars(trim($_POST['name'])); // Получение и очистка имени пользователя от лишних пробелов и специальных символов.
+    $email = htmlspecialchars(trim($_POST['email'])); // Получение и очистка email от лишних пробелов и специальных символов.
+    $message = htmlspecialchars(trim($_POST['message'])); // Получение и очистка сообщения от лишних пробелов и специальных символов.
 
-    if (!empty($name) && !empty($email) && !empty($message)) {
-        $dateTime = date('Y-m-d H:i:s');
-        $message = nl2br($message);
-        $entry = "$name|$email|$dateTime|$message\n";
-        file_put_contents($filePath, $entry, FILE_APPEND | LOCK_EX);
+    if (!empty($name) && !empty($email) && !empty($message)) { // Проверка, заполнены ли все поля (имя, email, сообщение).
+        $dateTime = date('Y-m-d H:i:s'); // Получение текущей даты и времени в формате 'ГГГГ-ММ-ДД ЧЧ:ММ:СС'.
+        $message = nl2br($message); // Преобразование символов новой строки в HTML-теги <br> для корректного отображения в браузере.
+        $entry = "$name|$email|$dateTime|$message\n"; // Формирование строки записи с разделителями (|) для сохранения в файл.
+        file_put_contents($filePath, $entry, FILE_APPEND | LOCK_EX); // Запись строки в файл, добавляя ее в конец файла и блокируя его для других процессов.
     }
 }
 
-if (file_exists($filePath)) {
-    $messages = array_reverse(file($filePath, FILE_IGNORE_NEW_LINES));
+if (file_exists($filePath)) { // Проверка, существует ли файл с сообщениями.
+    $messages = array_reverse(file($filePath, FILE_IGNORE_NEW_LINES)); // Чтение содержимого файла в массив, игнорируя символы новой строки, и переворот массива (чтобы последние сообщения были первыми).
+}
 }
